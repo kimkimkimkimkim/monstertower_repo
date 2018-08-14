7a1411cdfcb11b52e9816aba75dcde80fa282761
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2018-08-14 21:19:28 +0900

    18/08/14 commit

---
+++ MonsterTower/Assets/Script/FloarTextScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloarTextScript : MonoBehaviour {
+
+	//メンバ変数
+	private GameObject contentTower;
+	private GameObject ground;
+	private int floarNum = 0;
+
+	// Use this for initialization
+	void Start () {
+		contentTower = GameObject.Find ("ContentTower");
+		ground = GameObject.Find ("Ground");
+		while(contentTower.transform.GetChild(floarNum).gameObject != ground){
+			if (contentTower.transform.GetChild (floarNum).Find ("Wall").GetChild (0).gameObject ==
+			   this.gameObject) {
+				this.GetComponent<Text> ().text = floarNum.ToString ();
+			}
+			floarNum++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- MonsterTower/Assets/Script/HeroManager.cs
+++ MonsterTower/Assets/Script/HeroManager.cs
@@ -10,40 +10,45 @@ public class HeroManager : MonoBehaviour {
 
 
 	//メンバ変数
-	private float width = Screen.width;
+	//private float width = (float)Screen.width;
 	//private float height = Screen.height;
+	private float width;
 	private bool isRight = true;
 	private Vector3 posHero; //ヒーローの座標
+	private GameObject canvas;
 
 	// Use this for initialization
 	void Start () {
-		
+		canvas = GameObject.Find ("Canvas");
+		width = canvas.GetComponent<RectTransform> ().sizeDelta.x;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		posHero = this.GetComponent<RectTransform> ().localPosition;
-		Debug.Log (posHero);
 
 		//ヒーローを移動
 		if (isRight) {
-			if (posHero.x < width / 2 - 4) {
+			if (posHero.x < width/2 - 100) {
 				//右に移動
 				this.GetComponent<RectTransform> ().localPosition = new Vector3 (posHero.x + speed, posHero.y, posHero.z);
 			} else {
-				//左に移動
+				//左を向く
 				isRight = false;
+				transform.rotation = Quaternion.Euler(0, 180, 0);
 			}
 		} else {
-			if (posHero.x > -1 * (width / 2) + 4) {
+			if (posHero.x > -1 * (width/2) + 100) {
 				//左に移動
 				this.GetComponent<RectTransform> ().localPosition = new Vector3 (posHero.x - speed, posHero.y, posHero.z);
 			} else {
-				//右に移動
+				//右を向く
 				isRight = true;
+				transform.rotation = Quaternion.Euler(0, 0, 0);
 			}
 
 		}
 
 	}
+
 }
